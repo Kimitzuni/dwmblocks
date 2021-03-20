55f1cfc3c8c108790044bc6ef272e96af5845942
Rebecca <kimitxuni@gmail.com>
2021-03-20 11:28:00 +0000

    Update blocks.h

--- blocks.h
+++ blocks.h
@@ -2,7 +2,8 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	
-	{ " ", "~.scripts/pkgcheck.sh", 7200, 0 },
+	{ " ", "~/.scripts/pkgcheck.sh", 7200, 0 },
+//	{ "", "~/.scripts/battery.sh", 25, 0},  // Uncomment this if you use a laptop.
 	{ "", "~/.scripts/time.sh", 30, 0},
 };
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
